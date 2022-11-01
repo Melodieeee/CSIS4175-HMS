8a47fe5f4b946d07430d62fad62540d432c20494
Cindy Cheng <chengcindyy@gmail.com>
2022-10-31 21:58:12 -0700

    Forgot password: fix small bug and upload layout file

---
+++ app/src/main/java/com/example/howsMyStylist/ForgotPasswordActivity.java
@@ -0,0 +1,82 @@
+package com.example.howsMyStylist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText edit_email;
+    private Button btn_pwdReset, btn_cancel;
+    private FirebaseAuth authProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass_word);
+
+        edit_email = findViewById(R.id.edit_email);
+
+        // Submit forgot password requirement
+        btn_pwdReset = findViewById(R.id.btn_reset);
+        btn_pwdReset.setOnClickListener(v -> {
+
+            String email = edit_email.getText().toString();
+
+            if (TextUtils.isEmpty(email)){
+                Toast.makeText(ForgotPasswordActivity.this,
+                        "Please enter your registered email.", Toast.LENGTH_SHORT).show();
+                edit_email.setError("Email is required.");
+                edit_email.requestFocus();
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                Toast.makeText(ForgotPasswordActivity.this,
+                        "Please enter your valid email.", Toast.LENGTH_SHORT).show();
+                edit_email.setError("Valid email is required.");
+                edit_email.requestFocus();
+            } else {
+                Toast.makeText(ForgotPasswordActivity.this,
+                        "Clicked reset button", Toast.LENGTH_SHORT).show();
+                RestPassWord(email);
+
+            }
+        });
+        // return to mainActivity
+        btn_cancel = findViewById(R.id.btn_cancel);
+        btn_cancel.setOnClickListener(v -> startActivity(new Intent(ForgotPasswordActivity.this, MainActivity.class)));
+    }
+
+    private void RestPassWord(String email) {
+        authProfile = FirebaseAuth.getInstance();
+        authProfile.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(ForgotPasswordActivity.this,
+                            "Please check your inbox for password reset link", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(ForgotPasswordActivity.this, MainActivity.class);
+
+                    // Clear stack to prevent user coming back to ForgotPasswordActivity
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(ForgotPasswordActivity.this,
+                            "Something went wrong", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
